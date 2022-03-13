29c94f4a7cc7822510ec77a13b32ab2ba7541c10
xszefo <piotr.sumelka@gmail.com>
2022-03-13 18:12:05 +0100

    docker swarm

--- start-script.sh
+++ start-script.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
-if [ $1 -ne nodb ]
-then
-	until nc -v -z -w 5 db 3306
-	do
-		echo 'Waiting for db...'
-		sleep 5
-	done
-fi
+# if [ $1 -ne nodb ]
+# then
+# 	until nc -v -z -w 5 db 3306
+# 	do
+# 		echo 'Waiting for db...'
+# 		sleep 5
+# 	done
+# fi
+
+hostname > /code/restapp/templates/api/base.html
 
 python restapp/manage.py makemigrations
 python restapp/manage.py migrate
